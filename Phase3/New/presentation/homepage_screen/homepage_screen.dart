4a1d234582162e17334169738383cb07d8b70b02
ntua-el20889 <130045019+ntua-el20889@users.noreply.github.com>
2024-01-13 21:25:30 +0200

    Update homepage_screen.dart

--- Phase3/New/presentation/homepage_screen/homepage_screen.dart
+++ Phase3/New/presentation/homepage_screen/homepage_screen.dart
@@ -1,143 +1,159 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:swipeflix/core/app_export.dart';
+import 'package:swipeflix/database/database_manager.dart';
+import 'package:swipeflix/database/database_models.dart';
+import 'package:swipeflix/presentation/settings_screen/settings_screen.dart';
 import 'package:swipeflix/widgets/app_bar/appbar_iconbutton.dart';
 import 'package:swipeflix/widgets/app_bar/appbar_title_image.dart';
 import 'package:swipeflix/widgets/app_bar/custom_app_bar.dart';
 import 'package:swipeflix/widgets/custom_elevated_button.dart';
+import 'package:swipeflix/widgets/watchlist_button.dart';
+import 'package:swipeflix/presentation/lists_screen/lists_screen.dart';
+import 'package:swipeflix/presentation/searchpage_screen/searchpage_screen.dart';
+//import 'package:swipeflix/database/database_manager.dart';
 
-class HomepageScreen extends StatelessWidget {
-  const HomepageScreen({Key? key})
+var name = 'Spiderman';
+var link =
+    'https://store.playstation.com/store/api/chihiro/00_09_000/titlecontainer/SE/en/999/CUSA01163_00/image?w=800&h=800';
+
+class Homepage extends StatelessWidget {
+  Homepage({Key? key})
       : super(
           key: key,
         );
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: _buildAppBar(context),
-        body: SizedBox(
-          height: 567.v,
-          width: double.maxFinite,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              CustomImageView(
-                imagePath: ImageConstant.imgImage3,
-                height: 567.v,
-                width: 360.h,
-                alignment: Alignment.center,
+        return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Image(
+            image: AssetImage('assets/images/Logo_update.png'),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SettingsScreen()));
+              },
+              icon: Image(
+                image: AssetImage('assets/images/Icon button-darkSettings.png'),
               ),
-              Align(
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+            ),
+          ],
+          backgroundColor: Color(0xFF0D2C66),
+          toolbarHeight: 73.0,
+        ),
+        body: Stack(
+          children: [
+            // Movie Poster
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Image(
+                image: NetworkImage(link),
+              ),
+
+            ),
+            Column(
+              // Column Of Title -> (Like/Dislike & Navigation Buttons)
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  // Movie Title
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: Text(
+                    '$name',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimaryContainer,
+                        ),
+                  ),
+                ),
+                Column(
+                  // Column of Swipe and Navigation
                   children: [
-                    CustomElevatedButton(
-                      height: 35.v,
-                      text: "Spiderman",
-                      buttonStyle: CustomButtonStyles.fillPrimary,
-                      buttonTextStyle: theme.textTheme.titleLarge!,
+                    // Swipe And Bookmark
+                    Row(
+                      // Row of Swipe & Bookmark
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        // Dislike
+                        GestureDetector(
+                            onHorizontalDragUpdate: (details) {
+                              int sensitivity = 2;
+                              if (details.primaryDelta! > sensitivity) {
+                                // Swipe Left
+                              }
+                            },
+                            child: const Image(
+                                image: AssetImage(
+                                    'assets/images/DislikeAnimation.png'))),
+                        // Save
+                        WatchListButton(),
+                        // Like
+                        GestureDetector(
+                            onHorizontalDragUpdate: (details) {
+                              int sensitivity = 2;
+                              if (details.primaryDelta! < -sensitivity) {
+                                // Swipe Right
+                              }
+                            },
+                            child: const Image(
+                                image: AssetImage(
+                                    'assets/images/LikeAnimation.png'))),
+                      ],
                     ),
-                    Spacer(),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: 39.h,
-                        right: 35.h,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CustomImageView(
-                            imagePath: ImageConstant.imgSwipebuttonleft2,
-                            height: 58.adaptSize,
-                            width: 58.adaptSize,
-                          ),
-                          CustomImageView(
-                            imagePath: ImageConstant.imgBookmark,
-                            height: 37.v,
-                            width: 25.h,
-                            margin: EdgeInsets.only(
-                              top: 10.v,
-                              bottom: 11.v,
-                            ),
-                          ),
-                          CustomImageView(
-                            imagePath: ImageConstant.imgLikeanimation,
-                            height: 58.adaptSize,
-                            width: 58.adaptSize,
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 13.v),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: 100.h,
-                        right: 92.h,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CustomImageView(
-                            imagePath: ImageConstant.imgHome,
-                            height: 23.v,
-                            width: 21.h,
-                            margin: EdgeInsets.only(bottom: 7.v),
-                          ),
-                          Spacer(
-                            flex: 52,
-                          ),
-                          CustomImageView(
-                            imagePath: ImageConstant.imgMegaphone,
-                            height: 19.v,
-                            width: 24.h,
-                            margin: EdgeInsets.only(
-                              top: 5.v,
-                              bottom: 6.v,
-                            ),
-                          ),
-                          Spacer(
-                            flex: 47,
-                          ),
-                          CustomImageView(
-                            imagePath: ImageConstant.imgSearchicon,
-                            height: 30.v,
-                            width: 38.h,
-                          ),
-                        ],
-                      ),
+                    // Navigation Buttons
+                    Row(
+                      // Row of Navigation Buttons
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        // Home
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.home_outlined),
+                          color: Theme.of(context).colorScheme.secondary,
+                          iconSize: 35,
+                        ),
+                        // List
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ListsScreen()),
+                            );
+                          },
+                          icon: Icon(Icons.list_outlined),
+                          color: Theme.of(context).colorScheme.secondary,
+                          iconSize: 35,
+                        ),
+                        // Search
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SearchpageScreen()),
+                            );
+                          },
+                          icon: const Icon(Icons.search),
+                          color: Theme.of(context).colorScheme.secondary,
+                          iconSize: 35,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
-      ),
-    );
-  }
-
-  /// Section Widget
-  PreferredSizeWidget _buildAppBar(BuildContext context) {
-    return CustomAppBar(
-      centerTitle: true,
-      title: AppbarTitleImage(
-        imagePath: ImageConstant.imgTelevision,
-      ),
-      actions: [
-        Container(
-          margin: EdgeInsets.fromLTRB(15.h, 11.v, 15.h, 22.v),
-          decoration: AppDecoration.fillBlack.copyWith(
-            borderRadius: BorderRadiusStyle.circleBorder20,
-          ),
-          child: AppbarIconbutton(
-            imagePath: ImageConstant.imgSearch,
-          ),
-        ),
-      ],
-      styleType: Style.bgFill,
     );
   }
 }
