ee3216a7590a1af894a2a7a2bbc16b7b22ddb4e7
ntua-el20889 <130045019+ntua-el20889@users.noreply.github.com>
2024-01-14 17:47:24 +0200

    Update details_screen.dart

--- Phase3/New/presentation/details_screen/details_screen.dart
+++ Phase3/New/presentation/details_screen/details_screen.dart
@@ -1,49 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:swipeflix/core/app_export.dart';
+import 'package:swipeflix/presentation/homepage_screen/homepage_screen.dart';
+
+
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            _buildEight(context),
-            _buildEleven(context),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 7.v),
-                decoration: AppDecoration.fillErrorContainer,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 8.h, right: 69.h),
-                        child: Row(
-                          children: [
-                            // Genre containers...
-                            Container(
-                              width: 68.h,
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 16.h,
-                                vertical: 7.v,
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        // Detect downward swipe
+        if (details.primaryDelta! > 8) {
+          // Navigate to another page
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Homepage()),
+          );
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              _buildEight(context),
+              _buildEleven(context),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 7.v),
+                  decoration: AppDecoration.fillErrorContainer,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 8.h, right: 69.h),
+                          child: Row(
+                            children: [
+                              // Genre containers...
+                              Container(
+                                width: 68.h,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 16.h,
+                                  vertical: 7.v,
+                                ),
+                                decoration: AppDecoration.outlineIndigo.copyWith(
+                                  borderRadius: BorderRadiusStyle.roundedBorder8,
+                                ),
+                                child: Text(
+                                  "Sci-Fi",
+                                  style: theme.textTheme.titleSmall,
+                                  maxLines: 1, // Set maxLines to 1
+                                  overflow: TextOverflow.ellipsis,
+                                ),
                               ),
-                              decoration: AppDecoration.outlineIndigo.copyWith(
-                                borderRadius: BorderRadiusStyle.roundedBorder8,
-                              ),
-                              child: Text(
-                                "Sci-Fi",
-                                style: theme.textTheme.titleSmall,
-                                maxLines: 1, // Set maxLines to 1
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                               Container(
+                              Container(
                                 margin: EdgeInsets.only(left: 12.h),
                                 padding: EdgeInsets.symmetric(
                                   horizontal: 14.h,
@@ -59,14 +73,13 @@ class DetailsScreen extends StatelessWidget {
                                     fontSize: 17.0,
                                   ) ?? TextStyle(fontSize: 17.0),
                                   maxLines: 1, // Set maxLines to 1
-                                    overflow: TextOverflow.ellipsis,
+                                  overflow: TextOverflow.ellipsis,
                                 ),
                                 decoration: AppDecoration.outlineIndigo.copyWith(
                                   borderRadius: BorderRadiusStyle.roundedBorder8,
                                 ),
-                              
-                            ),
-                            Container(
+                              ),
+                              Container(
                                 margin: EdgeInsets.only(left: 12.h),
                                 padding: EdgeInsets.symmetric(
                                   horizontal: 14.h,
@@ -87,41 +100,70 @@ class DetailsScreen extends StatelessWidget {
                                 decoration: AppDecoration.outlineIndigo.copyWith(
                                   borderRadius: BorderRadiusStyle.roundedBorder8,
                                 ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 9.v),
+                      _buildSix(context),
+                      SizedBox(height: 10.v),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Your action when the button (image) is pressed
+                            print("Button (image) pressed!");
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(29.h),
                             ),
-                          ],
+                            padding: EdgeInsets.zero,
+                          ),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(29.h),
+                              color: AppDecoration.fillErrorContainer.color,
+                            ),
+                            child: CustomImageView(
+                              imagePath: ImageConstant.imgHome58x58,
+                              height: 58.adaptSize,
+                              width: 58.adaptSize,
+                              radius: BorderRadius.circular(29.h),
+                              alignment: Alignment.center,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 9.v),
-                    _buildSix(context),
-                    SizedBox(height: 10.v),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        height: 58.adaptSize,
-                        width: 58.adaptSize,
-                        decoration: AppDecoration.fillErrorContainer.copyWith(
-                          borderRadius: BorderRadiusStyle.roundedBorder28,
-                        ),
-                        child: CustomImageView(
-                          imagePath: ImageConstant.imgHome58x58,
-                          height: 100.adaptSize,
-                          width: 100.adaptSize,
-                          radius: BorderRadius.circular(29.h),
-                          alignment: Alignment.center,
-                        ),
-                      ),
-                    ),
-                  ],
+
+                      // Align(
+                      //   alignment: Alignment.bottomCenter,
+                      //   child: Container(
+                      //     height: 58.adaptSize,
+                      //     width: 58.adaptSize,
+                      //     decoration: AppDecoration.fillErrorContainer.copyWith(
+                      //       borderRadius: BorderRadiusStyle.roundedBorder28,
+                      //     ),
+                      //     child: CustomImageView(
+                      //       imagePath: ImageConstant.imgHome58x58,
+                      //       height: 100.adaptSize,
+                      //       width: 100.adaptSize,
+                      //       radius: BorderRadius.circular(29.h),
+                      //       alignment: Alignment.center,
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-
 
   /// Section Widget
   Widget _buildEight(BuildContext context) {
@@ -140,8 +182,10 @@ class DetailsScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 1.v,
-                width: 300.v,),
+                SizedBox(
+                  height: 1.v,
+                  width: 300.v,
+                ),
                 Text(
                   "Title of the Movie",
                   style: theme.textTheme.titleLarge,
@@ -181,7 +225,6 @@ class DetailsScreen extends StatelessWidget {
       ),
     );
   }
-
 
   Widget _buildSix(BuildContext context) {
     return Expanded(
